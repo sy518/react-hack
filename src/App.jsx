0b9bdb4d26434f09eb2406ccda71c0cdc8d6a95b
sy518 <samanazehra78692@gmail.com>
2025-06-24 17:38:40 -0700

    now

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetails from "./pages/ProductDetails";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
